7650166dd98613489feedb94275097ab989e38c7
fallaha <design0098@yahoo.com>
2020-12-06 10:31:46 +0330

    Slide 5

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-console.log(document.getElementById('sidebar-title'));
-let headerTitle = document.getElementById('sidebar-title');
-let header = document.getElementById('logo');
-console.log(headerTitle);
-headerTitle.textContent = 'Hello';
-headerTitle.innerText = 'Goodbye';
-console.log(headerTitle.innerText);
-headerTitle.innerHTML = '<h3>Hello Ali</h3>';
-header.style.borderBottom = 'solid 3px #000';
+let items = document.getElementsByClassName('box-title');
+console.log(items);
+console.log(items[1]);
+items[1].textContent = 'جاوااسکریپت';
+items[1].style.fontWeight = 'bold';
+items[1].style.backgroundColor = '#444444';
+items[1].style.color = '#fff';
+
+// Gives error
+//items.style.backgroundColor = '#f4f4f4';
+
+for(var i = 0; i < items.length; i++){
+  items[i].style.borderBottom = "1px solid red";
+}
